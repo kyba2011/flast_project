55a8f2abcd94e56d8e309a270c932830ef7cd013
kyba2011 <kylychba25@gmail.com>
2025-11-06 10:38:20 +0600

    flask + html

--- main.py
+++ main.py
@@ -8,16 +8,68 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return "Error 11093923919"
+def home():
+    return '''<h1>Welcome to the HTTP Status Codes Info Site</h1>
+    <a href="/errors">View HTTP Status Codes</a>
+    '''
+
+
+@app.route('/errors')
+def errors():
+    return '''
+    <h1>HTTP Status Codes</h1>
+    <ul>
+        <li><a href="/error/400">400 Bad Request</a></li>
+        <li><a href="/error/401">401 Unauthorized</a></li>
+        <li><a href="/error/403">403 Forbidden</a></li>
+        <li><a href="/error/404">404 Not Found</a></li>
+    </ul>
+    <a href="/about">About this site</a>
+    <br>
+    <a href="/">Back</a>
+    '''
 
 @app.route('/about')
 def about():
-    return "My name is Kylych"
+    return '''
+    <h1>About This Site</h1>
+    <p>This site provides information about common HTTP status codes.</p>
+    <a href="/errors">View HTTP Status Codes</a>
+    <a href="/">Back</a>
+    '''
 
-@app.route('/help')
-def help():
-    return "Use /about to learn more about me."
+@app.route('/error/400')
+def error_400():
+    return '''
+    <h1>400 Bad Request</h1>
+    <p>The server could not understand the request due to invalid syntax.</p>
+    <a href="/errors">Back</a>
+    '''
+
+@app.route('/error/401')
+def error_401():
+    return '''
+    <h1>401 Unauthorized</h1>
+    <p>The client must authenticate itself to get the requested response.</p>
+    <a href="/errors">Back</a>
+    '''
+
+@app.route('/error/403')
+def error_403():
+    return '''
+    <h1>403 Forbidden</h1>
+    <p>The client does not have access rights to the content.</p>
+    
+    <a href="/errors">Back</a>
+    '''
+
+@app.route('/error/404')
+def error_404():
+    return '''
+    <h1>404 Not Found</h1>
+    <p>The server can not find the requested resource.</p>
+    <a href="/errors">Back</a>
+    '''
 
 if __name__ == '__main__':
     app.run(debug=True) 
